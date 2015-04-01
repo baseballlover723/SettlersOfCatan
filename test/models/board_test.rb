142087ac4b62c3a3537f38e08e3dfd43be64669c
ctrokid <trowbrct@rose-hulman.edu>
2015-04-01 03:49:57 +0000

    added test cases to board_test

--- test/models/board_test.rb
+++ test/models/board_test.rb
@@ -7,15 +7,26 @@ module Catan
             @hexList = []
             @hexList << Catan::Hex.new(3, [3,2], :ore, ["left", "right"])
             @hexList << Catan::Hex.new(6, [1,1], :wool, ["left"])
-            @hexList << Catan::Hex.new(3, [3,2], :ore, ["left", "right"])
+            @hexList << Catan::Hex.new(3, [2,4], :ore, ["left", "right"])
             
             @game = Catan::Game.new(true, 2)
             @players = "Will be an array of Player objects"
         end
         
-        def test_that_board_instantiates
+        def test_that_board_instantiates_properly
             board = Catan::Board.new(@hexList, @game, @players)
             assert_instance_of Board, board
+            
+            assert_equal true, board.game.isPrivate
+            assert_equal [2,4], board.hexes[2].position
+        end
+        
+        def test_that_board_increments_turn
+            board = Catan::Board.new(@hexList, @game, @players)
+            assert_equal 1, board.currentTurn
+            
+            board.incrementTurn
+            assert_equal 2, board.currentTurn
         end
     end
 end
